b7a695811d0bc06088151d45317c7dd67e7a07e7
Mepivo <noemail@example.com>
2017-04-30 00:57:35 +1200

    added rgb video

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -30,16 +30,29 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tbOutput = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblConnectionID = new System.Windows.Forms.Label();
+            this.videoBox = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.videoBox)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(259, 82);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1221, 41);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(84, 37);
             this.button1.TabIndex = 0;
             this.button1.Text = "Send";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,23 +60,32 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(87, 82);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(1051, 42);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(164, 22);
+            this.textBox1.Size = new System.Drawing.Size(173, 22);
             this.textBox1.TabIndex = 1;
             // 
-            // richTextBox1
+            // tbOutput
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(15, 216);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(319, 176);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
+            this.tbOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOutput.Location = new System.Drawing.Point(967, 139);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(328, 432);
+            this.tbOutput.TabIndex = 2;
+            this.tbOutput.Text = "";
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Location = new System.Drawing.Point(964, 45);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 17);
             this.label1.TabIndex = 3;
@@ -71,25 +93,146 @@
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 196);
+            this.label2.Location = new System.Drawing.Point(964, 109);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Output:";
             // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Status";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ConnectionID";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(161, 13);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(13, 17);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "-";
+            // 
+            // lblConnectionID
+            // 
+            this.lblConnectionID.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblConnectionID.AutoSize = true;
+            this.lblConnectionID.Location = new System.Drawing.Point(161, 47);
+            this.lblConnectionID.Name = "lblConnectionID";
+            this.lblConnectionID.Size = new System.Drawing.Size(13, 17);
+            this.lblConnectionID.TabIndex = 9;
+            this.lblConnectionID.Text = "-";
+            // 
+            // videoBox
+            // 
+            this.videoBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.videoBox.Location = new System.Drawing.Point(38, 91);
+            this.videoBox.Name = "videoBox";
+            this.videoBox.Size = new System.Drawing.Size(920, 480);
+            this.videoBox.TabIndex = 10;
+            this.videoBox.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(164, 592);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(50, 21);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "rgb";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(314, 592);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(105, 21);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "DepthImage";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(485, 592);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(134, 21);
+            this.radioButton3.TabIndex = 13;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "SkeletalTracking";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(665, 592);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(58, 21);
+            this.radioButton4.TabIndex = 14;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Stop";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(346, 421);
+            this.ClientSize = new System.Drawing.Size(1311, 641);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.videoBox);
+            this.Controls.Add(this.lblConnectionID);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tbOutput);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.videoBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,9 +242,18 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox tbOutput;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblConnectionID;
+        private System.Windows.Forms.PictureBox videoBox;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -10,12 +10,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using System.Windows.Forms;
-
-
+using Microsoft.Kinect;
+using Coding4Fun.Kinect.WinForm;
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+
+        private KinectSensor kinect;
+
+        private DepthImagePixel[] depthPixels;
+        private byte[] colorPixels;
+
         public Form1()
         {
             InitializeComponent();
@@ -73,7 +79,182 @@ namespace WindowsFormsApplication1
                 str = ex.Message;
             }
 
-            richTextBox1.Text = str;
+            tbOutput.Text = str;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count <= 0)
+            {
+                MessageBox.Show("No kinect device found! Please connect kinect!");
+            }
+            else
+            {
+                kinect = KinectSensor.KinectSensors[0];
+                lblConnectionID.Text = kinect.DeviceConnectionId;
+                tbOutput.AppendText("Found kinectID: " + kinect.DeviceConnectionId + "\n");
+            }
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (radioButton1.Checked)
+            {
+                if (!kinect.IsRunning)
+                {
+                    kinect.Start();
+                    tbOutput.AppendText("Started Kinect!\n");
+                }
+                kinect.DepthStream.Disable();
+                kinect.SkeletonStream.Disable();
+                kinect.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                kinect.ColorFrameReady += Kinect_ColorFrameReady;
+            }
+            else if (radioButton2.Checked)
+            {
+                if (!kinect.IsRunning)
+                {
+                    kinect.Start();
+                    tbOutput.AppendText("Started Kinect!\n");
+                }
+                kinect.ColorStream.Disable();
+                kinect.SkeletonStream.Disable();
+                kinect.DepthStream.Enable();
+                kinect.DepthStream.Range = DepthRange.Near;
+                kinect.DepthFrameReady += Kinect_DepthFrameReady;
+                
+            }
+            else if (radioButton3.Checked)
+            {
+                if (!kinect.IsRunning)
+                {
+                    kinect.Start();
+                    tbOutput.AppendText("Started Kinect!\n");
+                }
+                kinect.ColorStream.Disable();
+                kinect.DepthStream.Disable();
+                kinect.SkeletonStream.Enable();
+                kinect.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                kinect.SkeletonFrameReady += Kinect_SkeletonFrameReady;
+            }
+            else
+            {
+                kinect.Stop();
+                tbOutput.AppendText("Stopped Kinect!\n");
+                lblConnectionID.Text = "-";
+                videoBox.Image = null;
+            }
+        }
+
+        private void Kinect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame f = e.OpenSkeletonFrame())
+            {
+                if (f != null)
+                {
+                    //videoBox.Image = CreateSkeletonTrackingMap(f);
+                }
+            }
+        }
+
+
+        private void Kinect_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame f = e.OpenDepthImageFrame())
+            {
+                if (f != null)
+                {
+                    videoBox.Image = CreateDepthBitMap(f);
+                }
+            }
+        }
+
+
+        private void Kinect_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using ( ColorImageFrame f = e.OpenColorImageFrame())
+            {
+                if (f != null)
+                {
+                    videoBox.Image = CreateBitMapFromSensor(f);
+                }
+            }
+        }
+
+        //private Bitmap CreateSkeletonTrackingMap(SkeletonFrame f)
+        //{
+        //    //list of skeletons
+        //    var skeletons = new Skeleton[f.SkeletonArrayLength];
+        //    f.CopySkeletonDataTo(skeletons);
+
+        //    //Find the first person to track
+        //    var trackedPerson = skeletons.FirstOrDefault(s => s.TrackingState == SkeletonTrackingState.Tracked);
+
+        //    if(trackedPerson !=null)
+        //    {
+        //        SkeletonPoint position = trackedPerson.Joints[JointType.Head].Position;
+        //        CoordinateMapper mapper = new CoordinateMapper(kinect);
+        //        var colorPoint = mapper.MapSkeletonPointToColorPoint(position, ColorImageFormat.InfraredResolution640x480Fps30);
+
+
+        //    }
+
+
+        //}
+
+        private Bitmap CreateBitMapFromSensor(ColorImageFrame frame)
+        {
+            var pixelData = new byte[frame.PixelDataLength];
+            frame.CopyPixelDataTo(pixelData);
+
+            var stride = frame.Width * frame.BytesPerPixel;
+            var bmpFrame = new Bitmap(frame.Width, frame.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            var bmpData = bmpFrame.LockBits(new Rectangle(0, 0, frame.Width, frame.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, bmpFrame.PixelFormat);
+            System.Runtime.InteropServices.Marshal.Copy(pixelData, 0, bmpData.Scan0, frame.PixelDataLength);
+
+            bmpFrame.UnlockBits(bmpData);
+            return bmpFrame;
+        }
+
+        //private Bitmap CreateDepthBitMap(DepthImageFrame f)
+        //{
+
+        //    depthPixels = new DepthImagePixel[kinect.DepthStream.FramePixelDataLength];
+        //    colorPixels = new byte[kinect.DepthStream.FramePixelDataLength * sizeof(int)];
+        //    f.CopyDepthImagePixelDataTo(this.depthPixels);
+
+        //    return depthPixels.TOBitMap
+
+        //    WriteableBitmap colorBitmap;
+
+        //    this.colorBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight,
+        //     96.0, 96.0, PixelFormats.Bgr32, null);
+
+
+        //}
+
+
+        private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            this.lblStatus.Text = kinect.Status.ToString();
+        }
+
+     
+        private void DisplayRGB()
+        {
+            kinect.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+            kinect.ColorFrameReady += Kinect_ColorFrameReady;
+        }
+
+        private void DisplayDepth()
+        {
+
+        }
+        
+        private void DisplaySkeletal()
+        {
+
         }
     }
 }
